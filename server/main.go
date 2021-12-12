627e1db9ceff859cc99724f7209a1f78dee7bb66
Jian <l059069045@gmail.com>
2021-12-12 20:40:52 +0800

    first commit

---
+++ server/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"strconv"
+)
+
+var todoList = []map[string]interface{}{
+	{"id": "0", "todo": "Buy bread"},
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/", showTodoList)
+	r.POST("/", createTodo)
+	r.POST("/deleteAll", deleteAllTodo)
+	r.POST("/delete", deleteTodo)
+	r.POST("/update", updateTodo)
+	r.Run()
+}
+
+var todoID = 1
+
+func updateTodo(c *gin.Context) {
+	if id := c.Request.FormValue("id"); id == "" {
+		c.String(500, fmt.Sprint("Error:", "id is empty"))
+	} else if todo := c.Request.FormValue("todo"); todo == "" {
+		c.String(500, fmt.Sprint("Error:", "todo is empty"))
+	} else {
+		for i, v := range todoList {
+			if v["id"] == id {
+				todoList[i]["todo"] = todo
+				c.JSON(200, todoList[i])
+				return
+			}
+		}
+		c.String(500, fmt.Sprint("Error:", "id not found"))
+	}
+}
+
+func deleteTodo(c *gin.Context) {
+	if id := c.Request.FormValue("id"); id == "" {
+		c.String(500, fmt.Sprint("Error:", "id is empty"))
+	} else {
+		for i, v := range todoList {
+			if v["id"] == id {
+				todoList = append(todoList[:i], todoList[i+1:]...)
+				fmt.Println(todoList)
+				c.String(200, fmt.Sprint("Success"))
+				return
+			}
+		}
+		c.String(500, fmt.Sprint("Error:", "id not found"))
+	}
+}
+
+func deleteAllTodo(c *gin.Context) {
+	todoList = []map[string]interface{}{}
+	c.String(200, fmt.Sprint("Success"))
+}
+
+func showTodoList(c *gin.Context) {
+	c.JSON(200, todoList)
+}
+
+func createTodo(c *gin.Context) {
+	if todo := c.Request.FormValue("todo"); todo == "" {
+		c.String(500, fmt.Sprint("Error:", "todo is empty"))
+	} else {
+		newTodo := map[string]interface{}{"id": strconv.Itoa(todoID), "todo": todo}
+		todoList = append(todoList, newTodo)
+		todoID++
+		fmt.Println(todoList)
+		c.JSON(200, newTodo)
+	}
+}
